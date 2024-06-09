e513375652becbc01be2973d5a8b84ece001fffe
Ramzi Osta <ramziosta@mac-mini.mynetworksettings.com>
2024-06-09 08:27:34 -0400

    edits

---
+++ scratch.js
@@ -0,0 +1,16 @@
+
+
+let s = "qwert";
+let d = "twerq";
+let arr1 =[];
+let arr2 = [];
+
+function compare (a, b) {
+
+        let str1 = a.split().sort().join('');
+        let str2 = b.split().sort().join('');
+
+        return str1 === str2;
+
+
+}
