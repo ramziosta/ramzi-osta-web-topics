47389a72ca87e2d0255b235c5fe52f953554f69d
Ramzi Osta <ramziosta@mac-mini.mynetworksettings.com>
2024-06-10 23:47:24 -0400

    media query ahnd extra eduits needed

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+
+
 let modal = document.getElementById("modal");
+let body = document.getElementById("body");
+let cardDisplay = document.getElementById("card-display");
 
-function toggleThem() {
+function toggleTheme() {
+    let currentTheme = body.getAttribute('data-theme');
+    let newTheme = currentTheme === 'light' || !currentTheme ? 'dark' : 'light';
+    body.setAttribute('data-theme', newTheme);
 
+    console.log("clicked color theme");
 }
 
 function toggleFavorites() {
     modal.style.display === "none" || modal.style.display === "" ?
-        modal.style.display = "block":
+        modal.style.display = "block" :
         modal.style.display = "none";
 }
 
@@ -14,3 +22,60 @@ function navigate() {
     console.log("clicked");
     window.location.href = "details.html";
 }
+
+window.onload = function (data) {
+    console.log("loaded");
+
+    let cardDisplay = document.getElementById("card-display");
+
+    data.forEach(item => {
+        let card = document.createElement("div");
+        let img = document.createElement("img");
+        let cardDetails = document.createElement("div");
+        let info1 = document.createElement("div");
+        let category = document.createElement("p");
+        let topic = document.createElement("h4");
+        let info2 = document.createElement("div");
+        let rating = document.createElement("p");
+        let author = document.createElement("p");
+
+        // Set attributes and content
+        img.src = `/assets/logos/${item.image}`;
+        img.alt = item.topic;
+        category.className = "category";
+        category.innerText = item.category;
+        topic.innerText = item.topic;
+        rating.innerText = `⭐⭐⭐️${item.rating}`;
+        author.innerHTML = item.name;
+        cardDetails.className = "card-details";
+        info1.className = "info-1";
+        info2.className = "info-2";
+        card.className = "card";
+        card.onclick = navigate; // Assuming navigate is a defined function
+        card.id = item.id;
+
+        // Append child elements
+        info1.appendChild(category);
+        info1.appendChild(topic);
+        info2.appendChild(rating);
+        info2.appendChild(author);
+        cardDetails.appendChild(info1);
+        cardDetails.appendChild(info2);
+        card.appendChild(img);
+        card.appendChild(cardDetails);
+        cardDisplay.appendChild(card);
+    });
+};
+
+async function fetchData() {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        window.onload(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// Fetch data and trigger onload
+fetchData();
